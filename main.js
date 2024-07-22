395159c7c687404ee490f31fc42569f9884b5585
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-22 11:04:48 +0800

    added me first kata of this series today

---
+++ main.js
@@ -0,0 +1,30 @@
+//NUMBER 
+
+/*
+
+*/                       
+
+//MY SOLUTION
+
+/*********THE ABOVE IS A TEMPLATE*************** */
+
+//NUMBER 1
+
+/*
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+65 --> 'A'
+97 --> 'a'
+48 --> '0
+*/
+
+//MY SOLUTION
+function getChar(c){
+    // ...
+    
+    return String.fromCharCode(c)
+  }
+
+
