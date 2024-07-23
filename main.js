05c5ba91a6b3c523509756727c9221f49fcdb398
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-24 00:07:02 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -8,6 +8,24 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 3
+
+/*
+Write a function that takes a single non-empty string of only lowercase and uppercase ascii letters (word) as its argument, and returns an ordered list containing the indices of all capital (uppercase) letters in the string.
+
+Example (Input --> Output)
+"CodEWaRs" --> [0,3,4,6]
+*/                       
+
+//MY SOLUTION
+var capitals = function (word) {
+  var caps = [];
+	for(var i = 0; i < word.length; i++) {
+    if(word[i].toUpperCase() == word[i]) caps.push(i);
+  }
+  return caps;
+};
+
 //NUMBER 2
 
 /*
