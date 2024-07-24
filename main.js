55cd84b528f8ea75a0e0c509fabe06af98142105
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-25 01:19:55 +0800

    added a new 6kyu kata

--- main.js
+++ main.js
@@ -6,7 +6,41 @@
 
 //MY SOLUTION
 
+
 /*********THE ABOVE IS A TEMPLATE*************** */
+
+//NUMBER 9
+
+/*
+Write a function that takes in a string of one or more words, and returns the same string, 
+but with all words that have five or more letters reversed (Just like the name of this Kata). 
+Strings passed in will consist of only letters and spaces. Spaces will be included only when 
+more than one word is present.
+
+Examples:
+
+"Hey fellow warriors"  --> "Hey wollef sroirraw" 
+"This is a test        --> "This is a test" 
+"This is another test" --> "This is rehtona test"
+*/                       
+
+//MY SOLUTION
+function spinWords(string){
+  //TODO Have fun :)
+  let notQualify = []
+  
+  let newString = string.split(' ')
+  
+  newString.forEach((word)=>{
+    if (word.length <= 4) {
+      notQualify.push(word)
+    }
+    else if (word.length > 4) {
+      notQualify.push(word.split('').reverse().join(''))
+    }
+    })
+  return notQualify.join(' ')
+}
 
 //NUMBER 8
 
