5f87a180fee1b8c4da17a5428797617d1dcab521
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-24 21:01:20 +0800

    added a 6kyu kata today

--- main.js
+++ main.js
@@ -8,6 +8,30 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+
+//NUMBER 6
+
+/*
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those 
+numbers in the form of a phone number.
+
+Example
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+
+Don't forget the space after the closing parentheses!
+*/                       
+
+//MY SOLUTION
+function createPhoneNumber(numbers){
+  let area = numbers.splice(0,3).join("")
+  //`(${area})`
+  
+  let phoneNumber = numbers.splice(0,3).join('')
+  
+  return `(${area}) ${phoneNumber}-${numbers.join('')}`
+}
+
 //NUMBER 5
 
 /*
@@ -26,7 +50,8 @@ findSimilarity("bag dog dig dot doog dogs","dog") should return "dog dig"
 findSimilarity("bag dog dig dot doog dogs","dig") should return "dog dig"
 findSimilarity("bag dog dig dot doog dogs","dot") should return "dot"
 findSimilarity("bag dog dig dot doog dogs","god") should return ""
-Hint: Use filter() will make your work easier; If you don't know how to solve the kata, please refer to the examples of lesson.
+Hint: Use filter() will make your work easier; If you don't know how to solve the kata, please refer to
+ the examples of lesson.
 */                       
 
 //MY SOLUTION
