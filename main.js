3a73f8dec68b8569d1adc640f412f396e052d67d
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-26 23:39:24 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -9,6 +9,33 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 13
+
+/*
+Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+ball1 = new Ball();
+ball2 = new Ball("super");
+
+ball1.ballType     //=> "regular"
+ball2.ballType     //=> "super"
+*/                       
+
+//MY SOLUTION
+class Ball {
+  constructor(ballType = "regular") {
+    this.ballType = ballType;
+  }
+}
+
+let ball1 = new Ball();
+let ball2 = new Ball("super");
+
+console.log(ball1.ballType); 
+console.log(ball2.ballType); 
+
 //NUMBER 12
 
 /*
