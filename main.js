aa4a809a0af7add8be128fc9587329c2e2a33fbb
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-27 23:22:05 +0800

    added a 6kyu kata today

--- main.js
+++ main.js
@@ -9,6 +9,33 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+
+//NUMBER 16
+
+/*
+Complete the solution so that it splits the string into pairs of two characters.
+ If the string contains an odd number of characters then it should replace the missing second 
+ character of the final pair with an underscore ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+*/                       
+
+//MY SOLUTION
+function solution(str){
+  let newStr = str.split('')
+  if (newStr.length % 2 !== 0) {
+    newStr.push('_')
+  }
+   let reception = []
+   for(let i = 0; i < newStr.length; i+=2) {
+       reception.push(newStr.slice(i, i + 2).join(''))
+   }
+   return reception
+}
+
 //NUMBER 15
 
 /*
