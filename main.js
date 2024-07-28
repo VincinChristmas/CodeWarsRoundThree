8397833c27fbf951643128ccfe22697597c4ce3c
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-28 12:34:53 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -9,6 +9,30 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 17
+
+/*
+Write a function named sumDigits which takes a number as input and returns the sum of the 
+absolute value of each of the number's decimal digits.
+
+For example: (Input --> Output)
+
+10 --> 1
+99 --> 18
+-32 --> 5
+Let's assume that all numbers in the input will be integer values.
+*/                       
+
+//MY SOLUTION
+function sumDigits(number) {
+  let newNum = Math.abs(number).toString().split('')
+  let sum = 0
+  newNum.forEach((sucker)=> {
+    sum = sum + Number(sucker)
+  })
+  return sum
+}
+
 
 //NUMBER 16
 
