619485bef1364d294b2f9bbf39765e20344708ef
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-28 19:55:35 +0800

    added a new kata

--- main.js
+++ main.js
@@ -9,6 +9,21 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+
+//NUMBER 18
+
+/*
+Create a combat function that takes the player's current health and the amount of damage recieved, 
+and returns the player's new health. Health can't be less than 0.
+*/                       
+
+//MY SOLUTION
+function combat(health, damage) {
+  // Write your code here
+  let newHealth = health - damage
+  return (newHealth >= 0) ? newHealth : 0
+}
+
 //NUMBER 17
 
 /*
