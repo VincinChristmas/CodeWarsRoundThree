8854d2093a0b2aa98c58fb50e20404e7dcad4cf8
VincinChristmas <VincinChristmas@hotmail.com>
2024-08-07 22:29:47 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -9,6 +9,32 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 29
+
+/*
+Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! 
+if name is not given (or passed as an empty String).
+
+Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+Examples:
+
+* With `name` = "john"  => return "Hello, John!"
+* With `name` = "aliCE" => return "Hello, Alice!"
+* With `name` not given 
+  or `name` = ""        => return "Hello, World!"
+*/                       
+
+//MY SOLUTION
+function hello(name) {
+  if (name == "" || name == null) {
+    return "Hello, World!"
+  }
+  else {
+  return `Hello, ${name[0].toUpperCase()}${name.slice(1).toLowerCase()}!`
+    }
+}
+
 
 //NUMBER 28
 
