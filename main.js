945f0b77aa73cca2aad7ee7480c57468c23a8698
VincinChristmas <VincinChristmas@hotmail.com>
2024-08-11 21:45:27 +0800

    added a new kata

--- main.js
+++ main.js
@@ -8,6 +8,42 @@
 
 
 /*********THE ABOVE IS A TEMPLATE*************** */
+
+//NUMBER 33
+
+/*
+Given a point in a Euclidean plane (x and y), return the quadrant the point
+ exists in: 1, 2, 3 or 4 (integer). x and y are non-zero integers, therefore the given point never lies on the axes.
+
+Examples
+(1, 2)     => 1
+(3, 5)     => 1
+(-10, 100) => 2
+(-1, -9)   => 3
+(19, -56)  => 4
+*/                       
+
+//MY SOLUTION
+function quadrant(x, y) {
+  switch (true) {
+      case (y > 0 && x > 0 ):
+      return 1;
+      break;
+      
+      case (x < 0 && y > 0):
+      return 2;
+      break;
+      
+      case (x < 0 && y < 0):
+      return 3;
+      break;
+      
+      case (x > 0 && y < 0):
+      return 4; 
+      break; 
+  }
+}
+
 //NUMBER 32
 
 /*
