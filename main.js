d3da351f0eb81de70a80faadaf7a1ee80a9f0a7f
VincinChristmas <VincinChristmas@hotmail.com>
2024-08-12 09:15:00 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -8,6 +8,31 @@
 
 
 /*********THE ABOVE IS A TEMPLATE*************** */
+//NUMBER 34
+
+/*
+Story
+Your online store likes to give out coupons for special occasions. Some customers try to cheat the system by entering invalid 
+codes or using expired coupons.
+
+Task
+Your mission:
+Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
+
+A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+
+*/                       
+
+//MY SOLUTION
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+  if (Date.parse(currentDate) <= Date.parse(expirationDate) && enteredCode === correctCode) {
+    return true
+  }
+  else {
+    return false
+  }
+}
+
 
 //NUMBER 33
 
