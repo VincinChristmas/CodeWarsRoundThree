317af4a6b152cc3fd6b1654ce937a26c9381faf0
VincinChristmas <VincinChristmas@hotmail.com>
2024-08-14 09:07:13 +0800

    completed a new kata today

--- main.js
+++ main.js
@@ -9,6 +9,20 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 36
+
+/*
+Grasshopper - Function syntax debugging
+A student was working on a function and made some syntax mistakes while coding. 
+Help them find their mistakes and fix them.
+*/                       
+
+//MY SOLUTION
+
+function main (verb, noun) {
+  return verb + noun
+}
+
 //NUMBER 35
 
 /*
