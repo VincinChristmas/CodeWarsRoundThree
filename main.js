0a0defe4232f9c439696961e9219a18c26a6c0c2
VincinChristmas <VincinChristmas@hotmail.com>
2024-08-18 17:08:02 +0800

    added a six kyu kata today

--- main.js
+++ main.js
@@ -9,6 +9,40 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 40
+
+/*
+Write a function that accepts a string, and returns the same string with all even indexed characters in each word upper 
+cased, and all odd indexed characters in each word lower cased. The indexing just explained is zero based, so the zero-ith
+ index is even, therefore that character should be upper cased and you need to start over for each word.
+
+The passed in string will only consist of alphabetical characters and spaces(' '). Spaces will only be present if there are 
+multiple words. Words will be separated by a single space(' ').
+
+Examples:
+"String" => "StRiNg"
+"Weird string case" => "WeIrD StRiNg CaSe"
+*/                       
+
+//MY SOLUTION
+function toWeirdCase(string){
+  //TODO
+  let emptyString = []
+  let strings = string.split(' ')
+  
+  strings.map((word)=>{
+    let newWord = word.split('')
+    
+    for(let i = 0; i < newWord.length; i++) {
+      if (i % 2 === 0) {
+        newWord[i] = newWord[i].toUpperCase()
+      }
+    }
+    emptyString.push(newWord.join(''))
+  })
+  return emptyString.join(' ')
+}
+
 //NUMBER 39
 
 /*
