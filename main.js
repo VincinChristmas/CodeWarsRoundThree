f5f07de65c5e823a4fcee5b48b5a323d749c44fc
VincinChristmas <VincinChristmas@hotmail.com>
2024-08-21 13:59:48 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -9,6 +9,24 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 44
+
+/*
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+Example:
+
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!"
+*/                       
+
+//MY SOLUTION
+var greet = function(name) {
+  let newName = name.toLowerCase()
+  let finalName = newName.replace(newName[0], newName[0].toUpperCase())
+  return `Hello ${finalName}!`
+};
+
 //NUMBER 43
 
 /*
