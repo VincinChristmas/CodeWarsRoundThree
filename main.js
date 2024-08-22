86924b52c66b476262e82f51f9f75624d848a62e
VincinChristmas <VincinChristmas@hotmail.com>
2024-08-22 13:53:09 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -9,6 +9,32 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 45
+
+/*
+There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), 
+calculate the total cost of the mangoes.
+
+Examples
+mango(2, 3) ==> 6    # 2 mangoes for $3 per unit = $6; no mango for free
+mango(3, 3) ==> 6    # 2 mangoes for $3 per unit = $6; +1 mango for free
+mango(5, 3) ==> 12   # 4 mangoes for $3 per unit = $12; +1 mango for free
+mango(9, 5) ==> 30   # 6 mangoes for $5 per unit = $30; +3 mangoes for free
+*/                       
+
+//MY SOLUTION
+function mango(quantity, price){
+  let sum = 0
+  let num = 0
+  while (sum < quantity) {
+    sum = sum + 1
+    if (sum % 3 == 0) {
+      num = num + 1
+    }
+  }
+    return (sum - num) * price
+  }
+
 //NUMBER 44
 
 /*
