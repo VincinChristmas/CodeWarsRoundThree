4bbd6ffef93a712c00abebf549f41b119b1504d1
VincinChristmas <VincinChristmas@hotmail.com>
2024-08-31 21:28:34 +0800

    added a 6kyu kata today

--- main.js
+++ main.js
@@ -9,6 +9,67 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 56
+
+/*
+Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
+
+a -> 1
+e -> 2
+i -> 3
+o -> 4
+u -> 5
+For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
+
+Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+
+For example, decode("h3 th2r2") would return "hi there".
+
+For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
+*/                       
+
+//MY SOLUTION
+function encode(string) {
+  let brokenString = string.split('')
+  for(let i = 0; i < brokenString.length; i++) {
+    if (brokenString[i] === 'a') {
+       brokenString[i] = 1
+    }
+     if (brokenString[i] === 'e') {
+       brokenString[i] = 2 
+    }
+    else if (brokenString[i] === 'i') {
+     brokenString[i] = 3
+    }
+    else if (brokenString[i] === 'o') {
+      brokenString[i] = 4
+    }
+   else if (brokenString[i] === 'u') {
+      brokenString[i] = 5
+    }
+    
+  }
+  return brokenString.join('')
+}
+
+function decode(string) {
+   const strArr = string.split('');
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i] === '1') {
+      strArr[i] = 'a';
+    } else if (strArr[i] === '2') {
+      strArr[i] = 'e';
+    } else if (strArr[i] === '3') {
+      strArr[i] = 'i';
+    } else if (strArr[i] === '4') {
+      strArr[i] = 'o';
+    } else if (strArr[i] === '5') {
+      strArr[i] = 'u';
+    }
+  }
+  return strArr.join('');
+}
+
 //NUMBER 55
 
 /*
