a07a2b47b6758cae0773c7e0b33897d14a2b347a
VincinChristmas <VincinChristmas@hotmail.com>
2024-09-03 22:07:55 +0800

    completed a kata today

--- main.js
+++ main.js
@@ -9,6 +9,33 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 59
+
+/*
+Your task is to find the nearest square number, nearest_sq(n) or nearestSq(n), of a positive integer n.
+
+For example, if n = 111, then nearest\_sq(n) (nearestSq(n)) equals 121, since 111 is closer to 121, the square of 11, than 100, the square of 10.
+
+If the n is already the perfect square (e.g. n = 144, n = 81, etc.), you need to just return n.
+
+Good luck :)
+
+*/                       
+
+//MY SOLUTION
+function nearestSq(n) {
+  let sqrtN = Math.sqrt(n);
+  let lowerSq = Math.floor(sqrtN) ** 2;
+  let upperSq = Math.ceil(sqrtN) ** 2;
+
+  if (Math.abs(lowerSq - n) < Math.abs(upperSq - n)) {
+    return lowerSq;
+  } else {
+    return upperSq;
+  }
+}
+
+
 //NUMBER 58
 
 /*
