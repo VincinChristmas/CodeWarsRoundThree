cd7315934f21d7778bdc99b5d3f533b4086ada15
VincinChristmas <VincinChristmas@hotmail.com>
2024-09-05 13:50:33 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -8,6 +8,32 @@
 
 
 /*********THE ABOVE IS A TEMPLATE*************** */
+//NUMBER 61
+
+/*
+Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+
+Examples:
+
+input:    output:
+0    ->   0
+2    ->   5
+3    ->   5
+12   ->   15
+21   ->   25
+30   ->   30
+-2   ->   0
+-5   ->   -5
+etc.
+Input may be any positive or negative integer (including 0).
+
+You can assume that all inputs are valid integers.
+*/                       
+
+//MY SOLUTION
+function roundToNext5(n){
+  return Math.ceil(n / 5) * 5;
+}
 
 //NUMBER 60
 
