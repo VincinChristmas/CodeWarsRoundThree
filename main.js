d9d2e918a39d4b4d453192188713e55037d25a22
VincinChristmas <VincinChristmas@hotmail.com>
2024-09-07 18:12:54 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -8,6 +8,49 @@
 
 
 /*********THE ABOVE IS A TEMPLATE*************** */
+
+//NUMBER 62
+
+/*
+Given a lowercase string that has alphabetic characters only and no spaces, return the highest value of consonant substrings. 
+Consonants are any letters of the alphabet except "aeiou".
+
+We shall assign the following values: a = 1, b = 2, c = 3, .... z = 26.
+
+For example, for the word "zodiacs", let's cross out the vowels. We get: "z o d ia cs"
+
+-- The consonant substrings are: "z", "d" and "cs" and the values are z = 26, d = 4 and cs = 3 + 19 = 22. The highest is 26.
+solve("zodiacs") = 26
+
+For the word "strength", solve("strength") = 57
+-- The consonant substrings are: "str" and "ngth" with values "str" = 19 + 20 + 18 = 57 and "ngth" = 14 + 7 + 20 + 8 = 49.
+*/                       
+
+//MY SOLUTION
+function solve(s) {
+  const charMap = {
+    a: 0, b: 2, c: 3, d: 4, e: 0, f: 6, g: 7, h: 8, i: 0, j: 10,
+    k: 11, l: 12, m: 13, n: 14, o: 0, p: 16, q: 17, r: 18, s: 19, t: 20,
+    u: 0, v: 22, w: 23, x: 24, y: 25, z: 26
+  };
+
+  let maxSum = 0;
+  let currentSum = 0;
+  let newString = s.toLowerCase().split('');
+
+  for (let i = 0; i < newString.length; i++) {
+    let currentValue = charMap[newString[i]];
+    if (currentValue !== 0) {
+      currentSum += currentValue;
+      maxSum = Math.max(maxSum, currentSum);
+    } else {
+      currentSum = 0; 
+    }
+  }
+
+  return maxSum;
+} 
+
 //NUMBER 61
 
 /*
