f45c981514619e8c79864d63532e4ced77854e33
VincinChristmas <VincinChristmas@hotmail.com>
2024-09-08 18:12:11 +0800

    completed an easy kata today

--- main.js
+++ main.js
@@ -9,6 +9,52 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+
+//NUMBER 63
+
+/*
+Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+Please use the following function names:
+
+addition = add
+
+multiply = multiply
+
+division = divide (both integer and float divisions are accepted)
+
+modulus = mod
+
+exponential = exponent
+
+subtraction = subt
+*/                       
+
+//MY SOLUTION
+function add(a,b){
+  return a + b
+}
+
+function divide(a,b){
+  return a / b
+}
+
+function multiply(a,b){
+  return a * b
+}
+
+function mod(a,b){
+  return a % b
+}
+ 
+function exponent(a,b){
+  return a ** b
+}
+  
+function subt(a,b){
+  return a - b
+}
+
 //NUMBER 62
 
 /*
