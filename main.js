9a93ccbdf49f1441f6e33603c716419fddadfed3
VincinChristmas <VincinChristmas@hotmail.com>
2024-09-13 09:13:21 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -8,6 +8,28 @@
 
 
 /*********THE ABOVE IS A TEMPLATE*************** */
+
+//NUMBER 67 
+
+/*
+JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+The solution would work like the following:
+
+getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+*/                       
+
+//MY SOLUTION
+
+function getEvenNumbers(numbersArray){
+  // filter out the odd numbers
+ function isEven(number) {
+   return number % 2 === 0
+ }
+  
+  return numbersArray.filter(isEven)
+ 
+}
 //NUMBER 66
 
 /*
