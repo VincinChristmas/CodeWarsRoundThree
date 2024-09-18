5c27e8f1de1c4f0f533344a3018ec5afb11795ad
VincinChristmas <VincinChristmas@hotmail.com>
2024-09-18 15:14:45 +0800

    added a new kata

--- main.js
+++ main.js
@@ -8,6 +8,33 @@
 
 
 /*********THE ABOVE IS A TEMPLATE*************** */
+
+//NUMBER 70
+
+/*
+Your task is simply to count the total number of lowercase letters in a string.
+
+Examples
+"abc" ===> 3
+
+"abcABC123" ===> 3
+
+"abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 3
+
+"" ===> 0;
+
+"ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 0
+
+"abcdefghijklmnopqrstuvwxyz" ===> 26
+*/                       
+
+//MY SOLUTION
+function lowercaseCount(str){
+  const lowercaseLetters = str.match(/[a-z]/g);
+  return lowercaseLetters? lowercaseLetters.length : 0;
+
+}
+
 //NUMBER 69
 
 /*
