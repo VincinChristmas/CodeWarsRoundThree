8ebf2f569c05770228dad74d3a14ce7316e1dc42
VincinChristmas <VincinChristmas@hotmail.com>
2024-09-29 10:17:06 +0800

    completed a new kata today

--- main.js
+++ main.js
@@ -9,6 +9,21 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 80
+
+/*
+Time to test your basic knowledge in functions! Return the odds from a list:
+
+[1, 2, 3, 4, 5]  -->  [1, 3, 5]
+[2, 4, 6]        -->  []
+*/                       
+
+//MY SOLUTION
+function odds(values){
+  // arrow it
+  return values.filter( numbers =>  numbers % 2 !== 0);
+}
+
 
 //NUMBER 79
 
