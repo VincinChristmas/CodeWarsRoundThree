f62c919ccc91bb54ea4034431116a47aab82333e
VincinChristmas <VincinChristmas@hotmail.com>
2024-09-30 10:33:44 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -9,6 +9,52 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+
+//NUMBER 81
+
+/*
+You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers.
+
+Your function will accept three arguments:
+The first and second argument should be numbers.
+The third argument should represent a sign indicating the operation to perform on these two numbers.
+
+if the variables are not numbers or the sign does not belong to the list above a message "unknown value" must be returned.
+
+Example:
+calculator(1,2,"+"); //=> result will be 3
+calculator(1,2,"&"); //=> result will be "unknown value"
+calculator(1,"k","*"); //=> result will be "unknown value"
+Good luck!
+*/                       
+
+//MY SOLUTION
+function calculator(a, b, sign) {
+  if (sign !== "+" && sign !== "-" && sign !== "*" && sign !== "/") {
+      return "unknown value";
+  }
+  if (typeof a !== "number" || typeof b !== "number") {
+      return "unknown value";
+  }
+
+  if (b == 0) {
+    return Infinity
+  }
+  switch (sign) {
+      case "+":
+          return a + b;
+      case "-":
+          return a - b;
+      case "*":
+          return a * b;
+      case "/":
+          if (b === 0) {
+              return "division by zero";
+          }
+          return a / b;
+  }
+}
+
 //NUMBER 80
 
 /*
