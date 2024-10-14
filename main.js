d1f8f6a13328a4afdfdce97c8013ad5a55d95a40
VincinChristmas <VincinChristmas@hotmail.com>
2024-10-13 21:39:01 -0500

    completed a new kata today

--- main.js
+++ main.js
@@ -8,6 +8,21 @@
 
 
 /*********THE ABOVE IS A TEMPLATE*************** */
+//NUMBER 87
+
+/*
+Given a number n, return the number of positive odd numbers below n, EASY!
+
+Examples (Input -> Output)
+7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+
+*/                       
+
+//MY SOLUTION
+function oddCount(n){
+  return Math.floor(n / 2)
+}
 
 
 //NUMBER 86
