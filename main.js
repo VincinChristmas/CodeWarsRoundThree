9fb233c1f4da4a499e464536aa9283174c0911b3
VincinChristmas <VincinChristmas@hotmail.com>
2024-10-14 23:59:56 -0500

    working ona new kata

--- main.js
+++ main.js
@@ -8,6 +8,21 @@
 
 
 /*********THE ABOVE IS A TEMPLATE*************** */
+
+
+function Hero (name) {
+  // add default values here
+  this.position='00'
+  this.health=100
+  this.damage= 5
+  this.experience=0
+  if (name === true) {
+    this.name = name
+  }
+  else {
+    this.name='Hero'
+  }
+}
 //NUMBER 87
 
 /*
