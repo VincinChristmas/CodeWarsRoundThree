a642571d871c2de5c24222a3fa5983c39895b4eb
VincinChristmas <VincinChristmas@hotmail.com>
2024-10-15 07:22:57 -0500

    added a new kata today

--- main.js
+++ main.js
@@ -9,20 +9,30 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 88
 
+/*
+Terminal Game - Create Hero Prototype
+In this first kata in the series, you need to define a Hero prototype to be used in a terminal game. The hero should have the following attributes:
+
+attribute	value
+name	user argument or 'Hero'
+position	'00'
+health	100
+damage	5
+experience	0
+*/                       
+
+//MY SOLUTION
 function Hero (name) {
   // add default values here
   this.position='00'
   this.health=100
   this.damage= 5
   this.experience=0
-  if (name === true) {
-    this.name = name
+  this.name = name || 'Hero'
   }
-  else {
-    this.name='Hero'
-  }
-}
+
 //NUMBER 87
 
 /*
