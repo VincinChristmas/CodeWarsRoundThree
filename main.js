e7a00679eaf119068b31f4151ae19030a08dd1e4
VincinChristmas <VincinChristmas@hotmail.com>
2024-10-19 10:05:45 -0500

    added a new kata today

--- main.js
+++ main.js
@@ -9,6 +9,25 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 90
+
+/*
+The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+But it's not working properly.
+
+Task
+Fix the bug so we can all go home early.
+
+Notes
+String str will never be null.
+*/                       
+
+//MY SOLUTION
+var replaceDots = function(str) {
+  return str.replace(/\./g, '-');
+}
+
 //NUMBER 89
 
 /*
