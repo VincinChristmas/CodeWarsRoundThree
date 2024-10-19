d24bd59d4997e2757587867fa66f3d7acd606d5c
VincinChristmas <VincinChristmas@hotmail.com>
2024-10-18 19:34:40 -0500

    added a new kata today

--- main.js
+++ main.js
@@ -9,6 +9,20 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 89
+
+/*
+Write a simple function that takes as a parameter a date object and returns a boolean value representing whether the date is today or not.
+
+Make sure that your function does not return a false positive by only checking the day.
+*/                       
+
+//MY SOLUTION
+function isToday(date) {
+  let today = new Date();
+  return today.toDateString() == date.toDateString() 
+}
+
 //NUMBER 88
 
 /*
