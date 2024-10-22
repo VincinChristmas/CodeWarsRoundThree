958a7f755f01a489719502a43d1e8dbef9e617db
VincinChristmas <VincinChristmas@hotmail.com>
2024-10-21 23:17:10 -0500

    added a new kata today

--- main.js
+++ main.js
@@ -8,6 +8,36 @@
 
 
 /*********THE ABOVE IS A TEMPLATE*************** */
+
+//NUMBER 92
+
+/*
+Given a logarithm base X and two values A and B, return a sum of logratihms with the base X: 
+log
+⁡
+X
+A
++
+log
+⁡
+X
+B
+log 
+X
+​
+ A+log 
+X
+​
+ B.
+*/                       
+
+//MY SOLUTION
+function logs(x , a, b){
+  //your code here
+    return Math.log(a * b) / Math.log(x);
+  }
+
+
 //NUMBER 91
 
 /*
