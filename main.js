7fdd62a19c3c2a39353855b933b0488ff47dee49
VincinChristmas <VincinChristmas@hotmail.com>
2024-10-27 09:33:27 -0500

    added a new kata today.

--- main.js
+++ main.js
@@ -12,6 +12,32 @@
 /*********THE ABOVE IS A TEMPLATE*************** */
 
 
+//NUMBER 98
+
+/*
+Hello, this is my first Kata so forgive me if it is of poor quality.
+
+In this Kata you should fix/create a program that returns the following values:
+
+false/False if either a or b (or both) are not numbers
+a % b plus b % a if both arguments are numbers
+You may assume the following:
+
+If a and b are both numbers, neither of a or b will be 0.
+*/                       
+
+//MY SOLUTION
+function myFirstKata(a, b) {
+  if (typeof(a) !== "number" || typeof(b) !== "number"){
+    return false
+  } 
+    
+   else {
+     return (a % b) + (b%a)
+    }
+}
+
+
 //NUMBER 97
 
 /*
