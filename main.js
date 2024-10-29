ebd6739757693d4b2779ca11308b36e35cb481e1
VincinChristmas <VincinChristmas@hotmail.com>
2024-10-29 17:00:38 -0500

    added a new kata today

--- main.js
+++ main.js
@@ -11,6 +11,33 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 99
+
+/*
+Write a function that calculates the original product price, without VAT.
+
+Example
+If a product price is 200.00 and VAT is 15%, then the final product price (with VAT) is: 200.00 + 15% = 230.00
+
+Thus, if your function receives 230.00 as input, it should return 200.00
+
+Notes:
+
+VAT is always 15% for the purposes of this Kata.
+Round the result to 2 decimal places.
+If null value given then return -1
+*/                       
+
+//MY SOLUTION
+function excludingVatPrice(price){
+  if (price === null) {
+    return -1
+  }
+  else {
+  return Number((price/1.15).toFixed(2));
+    }
+}
+
 
 //NUMBER 98
 
